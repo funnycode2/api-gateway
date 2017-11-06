5ec798317ea10ec2feacbe0c790b7e83436bb4b4
tiecheng <tczjhz@163.com>
2017-11-06 08:15:37 +0800

    初步添加管理页面

---
+++ src/goaway/web/url_mapping.go
@@ -0,0 +1,18 @@
+package web
+
+//管理界面接口定义
+
+//查询所有服务(按前缀模糊匹配/按过滤器名称/显示过滤器状态(是否启用)), 包括每个服务关联的过滤器
+// /admin/service/list?prefix=a&filtername=b&status=1
+
+//创建服务
+// /admin/service/create?
+
+//修改某个服务
+// /admin/service/modify?serviceid=1&
+
+//删除某个服务
+// /admin/service/delete?serviceid=1&
+
+//热配置(使网关配置生效)
+// /admin/service/reload
